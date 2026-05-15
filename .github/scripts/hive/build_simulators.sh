41c700dd1903ed06a9202bacf5129377802101ec
Soubhik Singha Mahapatra <soubhiksmp2004@gmail.com>
2026-05-15 14:05:24 +0530

    patch-hive

--- .github/scripts/hive/build_simulators.sh
+++ .github/scripts/hive/build_simulators.sh
@@ -6,7 +6,7 @@ fixture_variant="${1:-amsterdam}"
 case "${fixture_variant}" in
     amsterdam)
         eels_fixtures="https://github.com/ethereum/execution-specs/releases/download/tests-bal@v7.1.1/fixtures_bal.tar.gz"
-        eels_branch="devnets/bal/7"
+        eels_branch="fix/reth-bal-error-mapping"
         ;;
     osaka)
         eels_fixtures="https://github.com/ethereum/execution-spec-tests/releases/download/v5.3.0/fixtures_develop.tar.gz"
