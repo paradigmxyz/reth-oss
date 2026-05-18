9966a0586302e714b53280c029a524f8bafc3e5a
Soubhik Singha Mahapatra <soubhiksmp2004@gmail.com>
2026-05-18 14:23:18 +0530

    rerun

--- .github/scripts/hive/build_simulators.sh
+++ .github/scripts/hive/build_simulators.sh
@@ -6,7 +6,7 @@ fixture_variant="${1:-amsterdam}"
 case "${fixture_variant}" in
     amsterdam)
         eels_fixtures="https://github.com/ethereum/execution-specs/releases/download/tests-bal@v7.1.1/fixtures_bal.tar.gz"
-        eels_branch="fix/reth-bal-error-mapping"
+        eels_branch="fix/reth-bal-error-mapping" # temporary branch with a fix for the error mapping, should be removed once the fix is merged and released
         ;;
     osaka)
         eels_fixtures="https://github.com/ethereum/execution-spec-tests/releases/download/v5.3.0/fixtures_develop.tar.gz"
