67e60151f601bca03aba18835e1cda44e9814157
Soubhik Singha Mahapatra <soubhiksmp2004@gmail.com>
2026-06-03 16:55:12 +0530

    use rpc-only transfer logs in simulate call path

--- crates/rpc/rpc-eth-api/src/helpers/call.rs
+++ crates/rpc/rpc-eth-api/src/helpers/call.rs
@@ -203,30 +203,58 @@ pub trait EthCall: EstimateCall + Call + LoadPendingBlock + LoadBlock + FullEthA
                         }
                     };
 
-                    let inspector = TransferInspector::new(false);
-                    let evm =
-                        this.evm_config().evm_with_env_and_inspector(&mut db, evm_env, inspector);
-                    let mut builder = this.evm_config().create_block_builder(evm, &parent, ctx);
+                    let (result, results) = if trace_transfers {
+                        // Collect transfer operations without inserting synthetic logs into the
+                        // journal; they are appended only to the RPC simulation result below.
+                        let inspector = TransferInspector::new(false);
+                        let evm = this
+                            .evm_config()
+                            .evm_with_env_and_inspector(&mut db, evm_env, inspector);
+                        let mut builder = this.evm_config().create_block_builder(evm, &parent, ctx);
 
-                    if let Some(ref state_overrides) = state_overrides {
-                        simulate::apply_precompile_overrides(
-                            state_overrides,
-                            builder.evm_mut().precompiles_mut(),
+                        if let Some(ref state_overrides) = state_overrides {
+                            simulate::apply_precompile_overrides(
+                                state_overrides,
+                                builder.evm_mut().precompiles_mut(),
+                            )
+                            .map_err(|e| Self::Error::from_eth_err(EthApiError::other(e)))?;
+                        }
+
+                        simulate::execute_transactions(
+                            builder,
+                            &state_provider,
+                            calls,
+                            &mut remaining_call_gas_limit,
+                            chain_id,
+                            this.compute_state_root_for_eth_simulate(),
+                            this.converter(),
+                            true,
                         )
-                        .map_err(|e| Self::Error::from_eth_err(EthApiError::other(e)))?;
-                    }
+                        .map_err(map_err)?
+                    } else {
+                        let evm = this.evm_config().evm_with_env(&mut db, evm_env);
+                        let mut builder = this.evm_config().create_block_builder(evm, &parent, ctx);
 
-                    let (result, results) = simulate::execute_transactions(
-                        builder,
-                        &state_provider,
-                        calls,
-                        &mut remaining_call_gas_limit,
-                        chain_id,
-                        this.compute_state_root_for_eth_simulate(),
-                        this.converter(),
-                        trace_transfers,
-                    )
-                    .map_err(map_err)?;
+                        if let Some(ref state_overrides) = state_overrides {
+                            simulate::apply_precompile_overrides(
+                                state_overrides,
+                                builder.evm_mut().precompiles_mut(),
+                            )
+                            .map_err(|e| Self::Error::from_eth_err(EthApiError::other(e)))?;
+                        }
+
+                        simulate::execute_transactions(
+                            builder,
+                            &state_provider,
+                            calls,
+                            &mut remaining_call_gas_limit,
+                            chain_id,
+                            this.compute_state_root_for_eth_simulate(),
+                            this.converter(),
+                            false,
+                        )
+                        .map_err(map_err)?
+                    };
 
                     let simulated_header = result.block.clone_sealed_header();
                     db.override_block_hashes(BTreeMap::from([(
