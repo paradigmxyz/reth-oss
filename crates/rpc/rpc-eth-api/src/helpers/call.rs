7df6bc2e71d54dfea2201cb314d3a5337afb82ce
Soubhik Singha Mahapatra <soubhiksmp2004@gmail.com>
2026-06-03 18:46:04 +0530

    use transfer inspector for all simulate executions

--- crates/rpc/rpc-eth-api/src/helpers/call.rs
+++ crates/rpc/rpc-eth-api/src/helpers/call.rs
@@ -232,7 +232,10 @@ pub trait EthCall: EstimateCall + Call + LoadPendingBlock + LoadBlock + FullEthA
                         )
                         .map_err(map_err)?
                     } else {
-                        let evm = this.evm_config().evm_with_env(&mut db, evm_env);
+                        let inspector = TransferInspector::new(false);
+                        let evm = this
+                            .evm_config()
+                            .evm_with_env_and_inspector(&mut db, evm_env, inspector);
                         let mut builder = this.evm_config().create_block_builder(evm, &parent, ctx);
 
                         if let Some(ref state_overrides) = state_overrides {
