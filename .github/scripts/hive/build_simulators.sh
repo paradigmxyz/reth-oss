5529fbd02bc6b6cf412d6ef0ac77ce98a48deb6d
Soubhik Singha Mahapatra <soubhiksmp2004@gmail.com>
2026-08-11 17:33:07 +0530

    ci: use Glamsterdam devnet 8 EELS tests

--- .github/scripts/hive/build_simulators.sh
+++ .github/scripts/hive/build_simulators.sh
@@ -6,7 +6,7 @@ fixture_variant="${1:-amsterdam}"
 case "${fixture_variant}" in
     amsterdam)
         eels_fixtures="https://github.com/ethereum/execution-specs/releases/download/tests-glamsterdam-devnet@v8.1.0/fixtures_glamsterdam-devnet.tar.gz"
-        eels_branch="devnets/glamsterdam/8"
+        eels_branch="glamsterdam-devnet-8"
         eels_fork="Amsterdam"
         ;;
     osaka)
